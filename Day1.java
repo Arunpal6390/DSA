863c2be0bf0e9b9b1b9bdf9c2550fd65c9ae9f10
ARUN PAL <arunpal6390@gmail.com>
2023-12-27 12:18:55 +0530

    Initial commit

---
+++ Day1.java
@@ -0,0 +1,42 @@
+public class Day1 {
+    //static keyword to allow to call this method without creating object of the class
+    //void return type
+    //command line argument-|
+    //main is the function name,[] list
+    public static void main(String[] args) {
+        System.out.println("hello");
+//        System.out.println(args[0]);
+        //zero index values access using args[0]
+        //print function
+        // println function
+        //syntax of variable data_type v_name = value;
+        int money = 2000;
+        System.out.println(money);
+       /*
+       data types in java
+       there are two types of data types in java
+                        1.primitive data type-,
+                        byte,-1 byte/8 bits
+                        float,-4 bytes /32 bits
+                        double,-8 bytes/64 bits
+                        short ,- 2 bytes/16 bits
+                        int,-4 bytes/32 bit;
+                        long,-8 byte/ 64 bits integer
+                        boolean,- true ,false
+                        char,-2 bytes/ 16 bits
+       =>builtin provided by java
+       =>hold single value
+       =>can't be divided
+                          2.Non-primitive data types
+                          string,
+                          arrays,
+                          classes,
+                          interface;
+
+       =>user define
+       =>hold memory address of the variable
+       =>complex structure
+        */
+
+    }
+}
---
+++ Day2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Your Entered name is:"+name);
+
+        System.out.print("Enter lucky number:");
+        int num =sc.nextInt();
+        System.out.println("Your lucky number is:"+num);
+
+    }
+}
